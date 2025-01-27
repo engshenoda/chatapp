2520d7af5afceded044cb6e04e631866e713bec5
shenoda <engshenoda2020@gmail.com>
2025-01-27 19:29:03 +0200

    add chat cubit

---
+++ lib/cubits/chat_cubit/chat_cubit.dart
@@ -0,0 +1,33 @@
+import 'package:bloc/bloc.dart';
+import 'package:chatapp/constants.dart';
+import 'package:chatapp/models/message.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:meta/meta.dart';
+
+part 'chat_state.dart';
+
+class ChatCubit extends Cubit<ChatState> {
+  ChatCubit() : super(ChatInitial());
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(kMessagesCollections);
+  List<Message> messagesList = [];
+  void sendMessage({required String message, required String email}) {
+    try {
+      messages.add(
+        {kMessage: message, kCreatedAt: DateTime.now(), 'id': email},
+      );
+    } on Exception catch (e) {
+      print(e);
+    }
+  }
+
+  void getMessages() {
+    messages.orderBy(kCreatedAt, descending: true).snapshots().listen((event) {
+      messagesList.clear();
+      for (var doc in event.docs) {
+        messagesList.add(Message.fromJson(doc));
+      }
+      emit(ChatSuccess(messages: messagesList));
+    });
+  }
+}
